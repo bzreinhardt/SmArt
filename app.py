2f21fbfc6832777d13b1a1995ff78a5357b3c3e7
bzreinhardt <bzreinhardt@gmail.com>
2017-03-19 18:20:12 -0700

    update test

--- app.py
+++ app.py
@@ -1,62 +1,57 @@
-"""
-Flask Documentation:     http://flask.pocoo.org/docs/
-Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
-Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+#!/usr/bin/python
+from flask import Flask, url_for, json,  request 
+from scipy.spatial import distance
 
-This file creates your application.
-"""
+#DB jsonat 
+# [{name:foo, gps:(1,1), }, {} ...]
+DB_PATH='./art_db.json'
+METADATA = ['name', 'artist', 'instagram', 'notes']
 
-import os
-from flask import Flask, render_template, request, redirect, url_for
+DB = []
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+def add_to_db(info, db):
+  db.append(info)
+  with open(DB_PATH, 'w') as f:
+    f.write(json.dumps(db))   
+
+def find_closest_art(db, coords):
+  # Returns index of closest art piece
+  # This is the stupidest version. Actually looping
+  dists = []
+  for art in db:
+    dists.append(distance.cdist([(art['lat'],art['lon'])], coords))
+  return dists.index(min(dists))
 
 
-###
-# Routing for your application.
-###
+def load_db(db_path):
+  with open(DB_PATH, 'r') as f:
+    db = json.loads(f.read())
+  return db
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+  info = request.json
+  for datum in METADATA:
+    if not info.has_key(datum):
+      info[datum] = 'none'
+  add_to_db(info, DB)
+  return 'success'
+
+@app.route('/lookup', methods=['GET', 'POST'])
+def lookup_gps():
+  coords = [(request.json['lat'], request.json['lon'])]
+  index = find_closest_art(DB, coords)
+  return json.dumps({'name':DB[index]['name'], 'instagram':DB[index]['instagram']})
+
 
 @app.route('/')
-def home():
-    """Render website's home page."""
-    return render_template('home.html')
-
-
-@app.route('/about/')
-def about():
-    """Render the website's about page."""
-    return render_template('about.html')
-
-
-###
-# The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=600'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
-    return render_template('404.html'), 404
+def api_root():
+  return 'Welcome'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  DB = load_db(DB_PATH)
+  app.debug = True
+  app.run()
